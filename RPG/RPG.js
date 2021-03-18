d0fa7fd5ab8b484e34aba72988688172c120ddab
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2021-03-18 18:23:37 -0300

    moving WriteFileSync to RPG.js

--- RPG/RPG.js
+++ RPG/RPG.js
@@ -1,3 +1,8 @@
-module.exports = () => {
+module.exports = () => {/*
+    // Cria um arquivo para RPG Characters     rpg/character/serverID.json
+    if(!fs.existsSync(`./RPG/database/serverID_${raw.d.id}.json`)) {
+        fs.writeFileSync(`./RPG/database/serverID_${raw.d.id}.json`, "{}")
+        console.log(`\x1b[33mCriando arquivo \x1b[1m./RPG/database/serverID_${raw.d.id}.json\x1b[0m`)
+    }*/
 
 }
--- index.js
+++ index.js
@@ -27,11 +27,6 @@ client.on('raw', raw => {
             losa.save(server_config, server_config_path)
             console.log(`\x1b[32mNovo servidor adicionado: \x1b[1m${raw.d.name}\x1b[0m`)
         }
-        // Cria um arquivo para RPG Characters     rpg/character/serverID.json
-        if(!fs.existsSync(`./RPG/database/serverID_${raw.d.id}.json`)) {
-            fs.writeFileSync(`./RPG/database/serverID_${raw.d.id}.json`, "{}")
-            console.log(`\x1b[33mCriando arquivo \x1b[1m./RPG/database/serverID_${raw.d.id}.json\x1b[0m`)
-        }
     }
 })
 
