1869bcdf06c0d0a7966497675a62843854d5d9b8
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2021-03-19 00:11:37 -0300

    Create prefix
    = replace token.json to config.json
    + returns message if server is disabled
    + creating command to change prefix

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 const losa = require('./module/losa')
-const token = require('./token.json')
+const config = require('./config.json')
+const RPG = require('./RPG/RPG')
 
 const server_config_path = './server_config.json'
 
@@ -31,7 +32,17 @@ client.on('raw', raw => {
 })
 
 client.on('message', message => {
+    server_config = losa.load(server_config_path)
 
+    // Se server estiver false
+    if(server_config[`${message.channel.guild.id}`].status == false) return
+    if(message.content.toLowerCase().split(' ')[0] == 'kawobotsetprefix' && message.author.id == config.admin) {
+        server_config[message.channel.guild.id].prefix = message.content.split(' ')[1]
+        losa.save(server_config, server_config_path)
+        message.channel.send(`Prefix do server **${message.channel.guild.name}** alterado para **${message.content.split(' ')[1]}**`)
+        console.log(`\x1b[33mPrefix do server ${message.channel.guild.name} alterado para \x1b[32m${message.content.split(' ')[1]}\x1b[0m`)
+    }
+    RPG(message)
 })
 
-client.login(token.token)
+client.login(config.token)
