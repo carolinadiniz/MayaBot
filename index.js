1c97b2e1e2e53e2b493d49306dae2d63cd41fc6f
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2021-04-01 18:47:55 -0300

    Add react function  - update file
    + adding module update_embed
    + adding react function
    = changing functions to async

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const client = new Discord.Client()
 const fs = require('fs')
 const losa = require('./module/losa')
 const RPG = require('./RPG/RPG')
+const update_embeds = require('./RPG/module/update_embeds')
 // Json
 const config = require('./config.json')
 // Path
 const server_config_path = './server_config.json'
-
 
 
 // READY
@@ -17,7 +17,7 @@ client.on('ready', () => {
 
 
 // RAW
-client.on('raw', raw => {
+client.on('raw', async raw => {
     // Verifica se serverID está no banco de dados
     if(raw.t === 'GUILD_CREATE') {
         server_config = losa.load(server_config_path)
@@ -42,14 +42,36 @@ client.on('raw', raw => {
 })
 
 
+// ADD REACT
+client.on('messageReactionAdd', async (reaction, user) => {
+    // Retorna as mensagens de bot
+    //console.log()
+    if(user.bot) return
+
+    // Retorna se a mensagem não for do usuário
+    if(reaction.message.embeds[0].footer.text.split(' ')[3] !== user.id) return
+
+    // Message
+    await client.guilds.cache.get(reaction.message.channel.guild.id).channels.cache.get(reaction.message.channel.id).fetch(reaction.message.id)
+    .then(message => {
+        // Creating Message
+        const msg = message.messages.cache.get(reaction.message.id)
+
+        // Processing Message
+        update_embeds.newchar(client, msg, user, false)
+    })
+    .catch()
+})
+
+
 // MESSAGE
-client.on('message', message => {
+client.on('message', async message => {
     server_config = losa.load(server_config_path)
 
     // Se server estiver false
     if(server_config[`${message.channel.guild.id}`].status == false) return
     // Se a mensagem vier de um Bot
-    if(message.author.bot == true) return
+    //if(message.author.bot == true) return
     // Mudando Prefix
     if(message.content.toLowerCase().split(' ')[0] == 'kawobotsetprefix' && message.author.id == config.admin) {
         server_config[message.channel.guild.id].prefix = message.content.split(' ')[1]
@@ -59,7 +81,7 @@ client.on('message', message => {
     }
 
     // RPG
-    RPG(message, config)
+    await RPG(client, message, config)
 
     // Mostrando conteúdo das mensagens
     console.log(message.content)
