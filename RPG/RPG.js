545911eda661b0032342c1534fc0fc7ea65d14a4
Carolina Diniz (Kaworii) <phdsd.phdsd@gmail.com>
2021-04-01 18:32:25 -0300

    Updating RPG file (info char)
    + module update_embeds
    = functions to async
    + // Removing prefix from message
    = adding 'new char' to newchar command
    + client to function
    + command 'info char'

--- RPG/RPG.js
+++ RPG/RPG.js
@@ -1,8 +1,9 @@
 const losa = require('../module/losa')
 const add_channel = require('./module/add_channel')
 const new_character = require('./module/new_character')
+const update_embeds = require('./module/update_embeds')
 
-module.exports = (message, config) => {
+module.exports = async (client, message, config) => {
     // Variáveis
     author = message.author
     channelID = message.channel.id
@@ -13,12 +14,15 @@ module.exports = (message, config) => {
     if(message.content.split(' ')[0].includes(server_config[guildID].prefix)) {
         // Criando comando
         command = message.content.toLowerCase().split(' ')[0].slice(server_config[guildID].prefix.length)
+        // Removing prefix from message
+        message.content = message.content.substr(server_config[guildID].prefix.length)
 
 
         // Adicionando Canal RPG
         if(command == 'setchannelrpg' && author.id == config.admin) {
             add_channel.add(message)
         }
+
         // Removendo Canal RPG
         if(command == 'removechannelrpg' && author.id == config.admin) {
             add_channel.remove(message)
@@ -28,9 +32,13 @@ module.exports = (message, config) => {
         if(channelID != server_config[guildID].rpg.channel) return
 
         // Criar characters
-        if(command == 'newchar') {
-            new_character(message)
+        if(command == 'newchar' || message.content == 'new char') {
+            new_character(client, message)
         }
 
+        // Info
+        if(message.content == 'info char') {
+            update_embeds.infoChar(message)
+        }
     }
 }
